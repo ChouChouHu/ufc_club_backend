d9c7d04286f2e9ffe9376c7d5eb0682c99372ee1
AppWorks School <appworksschool@MacBook-Pro.local>
2024-02-27 10:20:54 +0800

    change text

--- apps/bot/src/data/comments.js
+++ apps/bot/src/data/comments.js
@@ -1,5 +1,4 @@
-export const TOO_MUCH_TOKEN =
-  '我只是個機器人，Code 太長了我吃不下！救命哪 @ChouChouHu';
+export const TOO_MUCH_TOKEN = 'Code 太長超過 token 限制了⋯⋯機器人公休 Zzzzzz';
 
 export const getAnwer = (res) => {
   return `> 我是 Alban，我只是一台機器人而已，我說的話參考就好，一切還是依導師的回饋為主。  
