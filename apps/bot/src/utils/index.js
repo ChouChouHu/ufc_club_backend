644ca964f9cf5ed2e80aeb62b6e51a369735897c
ChouChouHu <b02705003@ntu.edu.tw>
2024-05-19 17:32:19 +0800

    fixed: reverse the order for odds

--- apps/bot/src/utils/index.js
+++ apps/bot/src/utils/index.js
@@ -5,7 +5,7 @@ import { crawlOddsByPlayerNameFromCover, crawlAllPlayersByEventNumberFromUFC } f
 export async function getAllOddsByEventNumber(eventNumber) {
     const players = await crawlAllPlayersByEventNumberFromUFC(eventNumber);
     const allOdds = [];
-    for (let order = 0; order < 5; order++) {
+    for (let order = 4; order >= 0; order--) {
         const playerOne = players[order * 2].split(' ')[1].toLowerCase()
         const playerTwo = players[order * 2 + 1].split(' ')[1].toLowerCase()
         const odds = await crawlOddsByPlayerNameFromCover(eventNumber, [playerOne, playerTwo]);
